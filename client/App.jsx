63d3dd759541cc2f92faeb0b9d3ab2ea0f777716
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-08-09 16:37:31 -0700

    Render react app and implementing MVP routes from react-router-dom.

---
+++ client/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Switch, Route, } from 'react-router-dom';
+
+import CreateClass from './components/CreateClass';
+import SeatingChart from './components/SeatingChart';
+
+const App = props => {
+  return (
+    <div className="router">
+      <main className="main">
+        <Switch>
+          <Route exact path='/class' component={CreateClass} />
+          <Route exact path='/seatingchart' component={SeatingChart} />
+        </Switch>
+      </main>
+    </div>
+  );
+};
+
+export default App;
