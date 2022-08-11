a8b0d8a7468acfed404abc991050aaa1a527b7dd
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-08-10 22:00:50 -0700

    Add unordered list to Card component with all group members.

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -2,10 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Card = props => {
+
+  const list = props.members.map((member, index) => {
+    return (
+      <li key={'group' + index}>{member}</li>
+    );
+  });
+
   return (
     <div>
       <div>Group: {props.number}</div>
-      <div>Group members: {JSON.stringify(props.members)}</div>
+      <div>Group members:</div>
+      <ul>
+        {list}
+      </ul>
     </div>
   )
 }
