6a6d3719753c5e796ab2fca655b13f596ae04c3e
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-08-11 14:20:14 -0700

    Install bcrypt and add password hashing to mongoose pre middleware.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const bcrypt = require('bcryptjs');
+const SALT_WORK_FACTOR = 12;
+
 // if implmenting login, change so that username must be unique.
 const userSchema = new Schema({
   username: {type: String, required: true},
@@ -10,6 +13,12 @@ const userSchema = new Schema({
     period: String,
     roster: [String],
   }]
+});
+
+userSchema.pre('save', (next) => {
+  const hash = bcrypt.hashSync(this.password, SALT_WORK_FACTOR);
+  this.password = hash;
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
